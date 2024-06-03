34f1576c4664c706bb60272a282cd3b31e53a347
ajharul64 <ajharul.islam@barclays.com>
2024-06-03 14:37:27 +0100

    item routes codes

--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,21 @@
 const {Sequelize} = require('sequelize')
 const {sequelize} = require('../db')
 
-const Sauce = sequelize.define("sauces", {
+const Sauce = sequelize.define("sauce", {
   name: Sequelize.STRING,
+  image: Sequelize.STRING,
+});
+
+const Item = sequelize.define("item", {
+  name: Sequelize.STRING,
+  description: Sequelize.STRING,
+  price: Sequelize.FLOAT,
+  category: Sequelize.STRING,
   image: Sequelize.STRING,
 });
 
 module.exports = {
   db: sequelize,
   Sauce,
+  Item,
 };
